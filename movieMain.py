ac14b8caf08b71549b7c3f2421b847904e72ced9
kelly-wm24 <tanyx-wm24@student.tarc.edu.my>
2026-04-06 00:30:21 +0800

    Update movieMain.py

--- movieMain.py
+++ movieMain.py
@@ -236,7 +236,7 @@ st.markdown("""
 
 # --- 3. SIDEBAR NAVIGATION & FILTERS ---
 with st.sidebar:
-    st.image("https://cdn-icons-png.flaticon.com/512/3171/3171927.png", width:50) # Placeholder logo
+    st.image("https://cdn-icons-png.flaticon.com/512/3171/3171927.png", width:100) # Placeholder logo
     st.title("🍿 Cinema")
     st.markdown("---")
     
